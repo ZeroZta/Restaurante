2dc15700d92bfa5d2d3816819b008bb5b5ec9b9c
ZeroZta <alanriosarip@gmail.com>
2026-03-16 13:33:46 -0600

    Actualizar visuales.py

--- visuales.py
+++ visuales.py
@@ -46,12 +46,11 @@ while True:
     mouse_x, mouse_y = pygame.mouse.get_pos()
     rect_x = mouse_x - 200
     rect_y = mouse_y - 150
-    
+
     pygame.draw.rect(pantalla, ROSA, [rect_x, rect_y, 400, 300])
-    pygame.draw.rect(pantalla, VERDE, [rect_x, rect_y + 75, 200, 150])
-    pygame.draw.rect(pantalla, AMARILLO, [rect_x, rect_y + 112, 100, 75])
+    pygame.draw.rect(pantalla, VERDE, [rect_x+100, rect_y + 75, 200, 150])
+    pygame.draw.rect(pantalla, AMARILLO, [rect_x+150, rect_y + 112, 100, 75])
     pygame.display.flip()
-    
+
     # Mantener el juego a 60 FPS
     reloj.tick(60)
-
